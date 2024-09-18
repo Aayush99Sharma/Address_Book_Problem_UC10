52041207d3e95df089ee07db902a0e31815017f4
aayush <ayyush00sharma@gmail.com>
2024-09-18 11:10:21 +0530

    UC2: Implemented add contact functionality

--- address_book_main.py
+++ address_book_main.py
@@ -1,4 +1,4 @@
-# address_book_main.py
+# UC1 - Create a Contact Class
 
 class Contact:
     def __init__(self, first_name, last_name, address, city, state, zip_code, phone_number, email):
@@ -14,4 +14,21 @@ class Contact:
     def __str__(self):
         return f"{self.first_name} {self.last_name}, {self.address}, {self.city}, {self.state}, {self.zip_code}, {self.phone_number}, {self.email}"
 
-#UC2 
+#UC2 - add a contact
+
+def add_contact():
+    first_name = input("Enter First Name: ")
+    last_name = input("Enter Last Name: ")
+    address = input("Enter Address: ")
+    city = input("Enter City: ")
+    state = input("Enter State: ")
+    zip_code = input("Enter Zip Code: ")
+    phone_number = input("Enter Phone Number: ")
+    email = input("Enter Email: ")
+    contact = Contact(first_name, last_name, address, city, state, zip_code, phone_number, email)
+    return contact
+
+# Test adding a contact
+contact = add_contact()
+print(contact)
+
